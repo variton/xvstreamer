309331478932527fed6ff5af67c11db993e873ce
Robert King <varitonium@gmail.com>
2026-06-12 20:31:45 +0200

    XVS-6: dev-adding of static checking for unexpected wpr

--- include/core/error_hdr.h
+++ include/core/error_hdr.h
@@ -3,18 +3,18 @@
 #define ERROR_HDR_H
 
 #include <gst/gst.h>
-#include <errty.h>
 #include <error_info.h>
-
+#include <corety.h>
 #include <string>
 
 namespace core{
 
-template <typename ErrorType>
-auto unexpected(ErrorType error, GError * msg) {
-  std::string message = msg? msg->message : "unknown error";
-  return err::unexpected<ErrorType>(error,message);
+template <typename ErrorType, topology::HasCharMessage MsgType>
+auto unexpected(ErrorType error, MsgType* msg) {
+    std::string message = msg ? msg->message : "unknown error";
+    return err::unexpected<ErrorType>(error, message);
 }
+
 
 }
 
