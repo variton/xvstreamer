2bd6aab18ed54dea610654ccd237f889c0dbcb28
Robert King <varitonium@gmail.com>
2026-06-29 23:50:00 +0200

    XVS-27: doc-adding of corety.h doxygen documentation

--- include/topology/corety.h
+++ include/topology/corety.h
@@ -2,10 +2,23 @@
 #ifndef CORETY_H
 #define CORETY_H
 
+/**
+ * @file corety.h
+ * @brief Core topology type concepts.
+ */
+
 #include <concepts>
 
 namespace topology {
 
+/**
+ * @brief Concept for types exposing a C-string message member.
+ *
+ * A type satisfies this concept when `t.message` is convertible to
+ * `const char*`.
+ *
+ * @tparam T Type to evaluate.
+ */
 template <typename T>
 concept HasCharMessage = requires(T t) {
     { t.message } -> std::convertible_to<const char*>;
